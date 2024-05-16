d8ba5243787fee65107006793cd15ddc53d11863
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-05-16 10:47:41 +0500

    working with array

---
+++ Final Lab Practice.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <array>
+using namespace std;
+
+// int linearSearch(int arr[], int key, int index, int size) {
+//     if (index >= size) {
+//         return -1;
+//     }
+    
+//     if (arr[index] == key) {
+//         return index;
+//     }
+    
+//     return linearSearch(arr, key, index + 1, size);
+// }
+
+// int linearSearch2(array<int, 6> arr, int key, int index, int size) {
+//     if (index >= size) {
+//         return -1;
+//     }
+    
+//     if (arr[index] == key) {
+//         return index;
+//     }
+    
+//     return linearSearch2(arr, key, index + 1, size);
+// }
+
+int main() {
+    // passing array using builtIn method
+    // int arr[]{2, 3, 5, 6, 7, 1};
+    // int key;
+    // cout << "Enter a number to search its index: ";
+    // cin >> key;
+    // int size = sizeof(arr) / sizeof(arr[0]);
+    
+    
+    // int index = linearSearch(arr, key, 0 , size);
+    
+    
+    // if (index != -1) {
+    //     cout << "Element found at " << index;
+    // } else {
+    //     cout << "Element not found";
+    // }
+    
+    // passing array using library defined method
+    // array<int, 6> arr{2, 3, 5, 6, 7, 1};
+    // int key;
+    // cout << "Enter a number to search its index: ";
+    // cin >> key;
+    // int size = arr.size();
+    
+    
+    // int index = linearSearch2(arr, key, 0 , size);
+    
+    
+    // if (index != -1) {
+    //     cout << "Element found at " << index;
+    // } else {
+    //     cout << "Element not found";
+    // }
+    
+}
